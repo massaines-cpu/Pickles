a2a128ab9b08b7d534f765adf846ec48d26c90d8
NoemIADev <majerusnoemie@gmail.com>
2026-01-15 16:49:16 +0100

    ca marche

--- PicklesAPI.py
+++ PicklesAPI.py
@@ -1,4 +1,4 @@
-from uvicorn import run
+import uvicorn
 from fastapi import FastAPI
 from typing import Optional
 from pydantic import BaseModel
@@ -7,44 +7,61 @@ import psycopg
 app = FastAPI()
 
 class LivreCreate(BaseModel):
-    Titre: str                                # Titre *
-    Auteur: str                           # Auteur *
-    Genre: str                                # Genre *
-    Serie: Optional[str] = None               # Série
-    Edition: str                              # Edition *
-    Année: int                    # Année (de publication) *
-    Editeur: str                              # Editeur *
-    Etat: str                                 # Etat *
-    Exemplaire: int                           # Exemplaire *
-    ISBN: int                                 # ISBN *
-    Résumé: Optional[str] = None              # Résumé
-#class Auteurs (BaseModel):
+    titre: str                                # Titre *
+    #auteur: str                           # Auteur *
+    #Genre: str                                # Genre *
+    serie_id: Optional[int] = None               # Série
+    edition_id: int                              # Edition *
+    annee: int                    # Année (de publication) *
+    #Editeur: str                              # Editeur *
+    #Etat: str                                 # Etat *
+    #Exemplaire: int                           # Exemplaire *
+    #ISBN: int                                 # ISBN *
+    resume: Optional[str] = None              # Résumé
+    #class Auteurs (BaseModel):
    # Auteur: str 
 
-DB_DSN = "postgresql://postgres:sandy30@localhost:5432/ma_db"
+def get_conn():
+    return psycopg.connect(
+        dbname="ma-mb",
+        user="postgres",
+        password="sandy30",
+        host="127.0.0.1",
+        port=5432
+    )
 
 
 #@app.post("/auteurs"])
 #async def lister_auteurs():
-@app.post("/livres")
+@app.post("/livreS")
 async def ajouter_livre(livre: LivreCreate):
     # convertir livre en sql et l'ajouter à la base de données
     
     data = livre.model_dump()
 
     sql = """
-    INSERT INTO livres
-    (titre, auteur, genre, serie, edition, annee_publication, editeur, etat, exemplaire, isbn, resume)
+    INSERT INTO livre
+    (titre, serie_id, edition_id, annee, resume)
     VALUES
-    (%(titre)s, %(auteur)s, %(genre)s, %(serie)s, %(edition)s,
-     %(annee_publication)s, %(editeur)s, %(etat)s, %(exemplaire)s,
-     %(isbn)s, %(resume)s)
+    (%(titre)s,  %(serie_id)s, %(edition_id)s,
+     %(annee)s, %(resume)s)
     RETURNING id;
     """
-    with psycopg.connect(DB_DSN) as conn:
-        with conn.cursor() as cur:
-            cur.execute(sql, data)
-            new_id = cur.fetchone()[0]
-            conn.commit()
-
+   
+   
+    conn = get_conn()
+    with conn.cursor() as cur:
+        cur.execute(sql, data)
+        new_id = cur.fetchone()[0]
+        conn.commit()  
+        conn.close()
     return {"id": new_id, **data}
+
+@app.get("/auteurs")
+def get_auteurs():
+    conn = get_conn()
+    with conn.cursor() as cur:
+        cur.execute("SELECT * FROM Auteur")
+        res = cur.fetchall()
+    conn.close()
+    return res
