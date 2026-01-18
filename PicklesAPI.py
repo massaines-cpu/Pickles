ebc4f4cefcf585cc6c023427f1d19992dd0ce4a8
noemie majerus <majerusnoemi@gmail.com>
2026-01-18 21:17:52 +0100

    commit de secu

--- PicklesAPI.py
+++ PicklesAPI.py
@@ -4,42 +4,65 @@ from typing import Optional
 from pydantic import BaseModel
 import psycopg
 
-app = FastAPI()
+app = FastAPI(debug=True)
 
+#se que je recois du formulaire livre
 class LivreCreate(BaseModel):
     titre: str                                # Titre *
-    #auteur: str                           # Auteur *
-    #Genre: str                                # Genre *
-    serie_id: Optional[int] = None               # Série
-    edition_id: int                              # Edition *
-    annee: int                    # Année (de publication) *
-    #Editeur: str                              # Editeur *
-    #Etat: str                                 # Etat *
-    #Exemplaire: int                           # Exemplaire *
-    #ISBN: int                                 # ISBN *
+    auteur: str                                # Auteur *
+    Genre: str                                # Genre *
+    serie: Optional[str] = None               # serie
+    edition: int                              # Edition *
+    annee: int                                # Année (de publication) *
+    Editeur: str                              # Editeur *
+    Etat: str                                 # Etat *
+    Exemplaire: int                           # Exemplaire *
+    ISBN: int                                 # ISBN *
     resume: Optional[str] = None              # Résumé
-    #class Auteurs (BaseModel):
-   # Auteur: str 
+    
+    #CE QUE JE RECOIS DU FORMUALIRE cree un amie
+    class Amis(BaseModel):
+        nom: str
+        telephone: int
+        ecole: str
 
+    #formulaire preté un livre
+
+#CONEXION A LA BASE
 def get_conn():
     return psycopg.connect(
-        dbname="ma-mb",
+        dbname="ma_db",
         user="postgres",
         password="sandy30",
         host="127.0.0.1",
         port=5432
     )
+    
+def get_or_create(cursor, table, nom):
+    #"""Cherche une entrée dans table par nom, sinon l'insère et retourne l'id"""
+    cursor.execute(f"SELECT id FROM {table} WHERE nom = %s", (nom,))
+    row = cursor.fetchone()
+    if row:
+        return row[0]
+    cursor.execute(f"INSERT INTO A {table} (nom) VALUES (%s) RETURNING id", (nom,))
+    return cursor.fetchone()[0]
 
 
-#@app.post("/auteurs"])
-#async def lister_auteurs():
+
+
+##########POST#####
+
+
 @app.post("/livreS")
 async def ajouter_livre(livre: LivreCreate):
+    conn = get_conn()
+    cursor = conn.cursor()
     # convertir livre en sql et l'ajouter à la base de données
+    edition_id = get_or_create(cursor , "edition" ,livre.edition)
     
     data = livre.model_dump()
 
-    sql = """
+    sql1 = """
     INSERT INTO livre
     (titre, serie_id, edition_id, annee, resume)
     VALUES
@@ -49,9 +72,20 @@ async def ajouter_livre(livre: LivreCreate):
     """
    
    
+
+
+
+
+
+
+
+
+################GET#############
+
+
     conn = get_conn()
     with conn.cursor() as cur:
-        cur.execute(sql, data)
+        cur.execute(sql1, data)
         new_id = cur.fetchone()[0]
         conn.commit()  
         conn.close()
@@ -61,7 +95,31 @@ async def ajouter_livre(livre: LivreCreate):
 def get_auteurs():
     conn = get_conn()
     with conn.cursor() as cur:
-        cur.execute("SELECT * FROM Auteur")
+        cur.execute("SELECT nom FROM Auteur")
+        
         res = cur.fetchall()
+        conn.commit()
+        conn.close()
+        print("voici la liste", res)
+    return [{"nom": r[0]} for r in res]
+
+@app.get("/EDITION")
+def get_edition():
+    conn = get_conn()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM Edition")
+    rows = cursor.fetchall()
+    cursor.close()
     conn.close()
-    return res
+    return [{"id": r[0], "nom": r[1]} for r in rows]
+
+@app.get("/Ami")
+def get_ami():
+    conn = get_conn()
+    cursor = conn.cursor()
+    cursor.execute("SELECT nom FROM ")
+    rows = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return [{"id": r[0], "nom": r[1]} for r in rows]
+
